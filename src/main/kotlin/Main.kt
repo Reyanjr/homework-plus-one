7caf3e2ae0ea3af9e6c39f05300473af34e99d74
Reyan Jahangir <reyanjr@mail.ru>
2024-03-14 09:55:46 +0300

    Transpose Matrix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
-fun main(args: Array<String>) {
-    val solution = Solution()
-    solution.plusOne(intArrayOf(9, 9, 9))
-    println()
+fun main (args: Array<String>) {
+    class Solution {
+        fun transpose(matrix: Array<IntArray>): Array<IntArray> {
+            val ans: Array<IntArray> = Array(matrix[0].size) { IntArray(matrix.size) { 0 } }
+            for (i: Int in 0 until matrix.size) {
+                for (j: Int in 0 until matrix[0].size) {
+                    ans[j][i] = matrix[i][j]
+                }
+            }
+
+            return ans
+        }
+    }
 }
--- src/main/kotlin/solution 1.kt
+++ src/main/kotlin/solution 1.kt
@@ -1,30 +1,36 @@
-class `solution 1` {
-    fun transpose(matrix: Array<IntArray>): Array<IntArray> {
-        val numRows = matrix.size
-        val numCols = matrix[0].size
+class solution {
+fun transpose(matrix: Array<IntArray>): Array<IntArray> {
+    val rows = matrix.size
+    val cols = matrix[0].size
 
-        val transposedMatrix = Array(numCols) { IntArray(numRows) }
+    val transposedMatrix = Array(cols) { IntArray(rows) }
 
-        for (i in 0 until numRows) {
-            for (j in 0 until numCols) {
-                transposedMatrix[j][i] = matrix[i][j]
-            }
-        }
-
-        return transposedMatrix
-    }
-
-    fun main() {
-        val matrix = arrayOf(
-            intArrayOf(1, 2, 3),
-            intArrayOf(4, 5, 6)
-        )
-
-        val transposedMatrix = transpose(matrix)
-
-        for (row in transposedMatrix) {
-            println(row.joinToString())
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            transposedMatrix[j][i] = matrix[i][j]
         }
     }
 
+    return transposedMatrix
+}
+
+fun main() {
+    val matrix = arrayOf(
+        intArrayOf(1, 2, 3),
+        intArrayOf(4, 5, 6),
+        intArrayOf(7, 8, 9)
+    )
+
+    val transposedMatrix = transpose(matrix)
+
+    println("Original Matrix:")
+    for (row in matrix) {
+        println(row.joinToString())
+    }
+
+    println("\nTransposed Matrix:")
+    for (row in transposedMatrix) {
+        println(row.joinToString())
+    }
+ }
 }
