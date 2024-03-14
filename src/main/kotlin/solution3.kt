cff33e18f25061a6fe67ffd3ad3a09b2f3180f66
Reyan Jahangir <reyanjr@mail.ru>
2024-03-14 10:24:15 +0300

    Search Insert Position

---
+++ src/main/kotlin/solution3.kt
@@ -0,0 +1,16 @@
+class solution3 {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var l = 0
+        var r = nums.size - 1
+        while (l <= r) {
+            val mid = (l + r) / 2
+            when {
+                target < nums[mid] -> r = mid -1
+                target > nums[mid] -> l = mid +1
+                target == nums[mid] -> return  mid
+            }
+
+        }
+        return l
+    }
+}
