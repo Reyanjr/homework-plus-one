b627fb49b3ae207391aa07909aeb88a056dd867c
Reyan Jahangir <reyanjr@mail.ru>
2024-03-17 16:24:17 +0300

    First Bad Version

--- src/main/kotlin/solution 4.kt
+++ src/main/kotlin/solution 4.kt
@@ -1,38 +1,35 @@
-class `solution 4` {
-    fun isValidSudoku(board: Array<CharArray>): Boolean {
-        val seen = HashSet<String>()
+class `Solution 4` {
+    / Define a global variable to simulate the first bad version
+    var firstBadVersion = 4
 
-        for (i in 0 until 9) {
-            for (j in 0 until 9) {
-                val value = board[i][j]
-                if (value != '.') {
-                    if (!seen.add("$value в строке $i") ||
-                        !seen.add("$value в столбце $j") ||
-                        !seen.add("$value в подсети ${i / 3}-${j / 3}")
-                    ) {
-                        return false
-                    }
-                }
+    // Function to simulate the provided API call to determine if a version is bad
+    fun isBadVersion(version: Int): Boolean {
+        // Check if the version is bad based on the simulated first bad version
+        return version >= firstBadVersion
+    }
+
+    fun firstBadVersion(n: Int): Int {
+        var left = 1
+        var right = n
+
+        while (left < right) {
+            val mid = left + (right - left) / 2
+
+            if (isBadVersion(mid)) {
+                right = mid // The bad version is in the current range
+            } else {
+                left = mid + 1 // The bad version is after the current range
             }
         }
 
-        return true
+        return left
     }
 
     fun main() {
-        val board = arrayOf(
-            charArrayOf('5','3','.','.','7','.','.','.','.'),
-            charArrayOf('6','.','.','1','9','5','.','.','.'),
-            charArrayOf('.','9','8','.','.','.','.','6','.'),
-            charArrayOf('8','.','.','.','6','.','.','.','3'),
-            charArrayOf('4','.','.','8','.','3','.','.','1'),
-            charArrayOf('7','.','.','.','2','.','.','.','6'),
-            charArrayOf('.','6','.','.','.','.','2','8','.'),
-            charArrayOf('.','.','.','4','1','9','.','.','5'),
-            charArrayOf('.','.','.','.','8','.','.','7','9')
-        )
-
-        println(isValidSudoku(board)) // Output: true
+        // Example usage
+        val n = 5 // Total number of versions
+        val badVersion = firstBadVersion(n) // Finding the first bad version
+        println("The first bad version is at version $badVersion")
     }
 
 }
