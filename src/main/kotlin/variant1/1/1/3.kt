0f01338a7c42ed5dd7a3e1178232f8cde3fef195
Reyan Jahangir <reyanjr@mail.ru>
2024-04-13 13:17:34 +0300

    3.Использование `super`.

---
+++ src/main/kotlin/variant1/1/1/3.kt
@@ -0,0 +1,26 @@
+package variant1.`1`.`1`
+
+class `3` {
+    open class Vehicle(val speed: Int, val color: String) {
+        open fun displayInfo() {
+            println("Vehicle - Speed: $speed, Color: $color")
+        }
+    }
+
+    class Car(speed: Int, color: String, val numDoors: Int) : Vehicle(speed, color) {
+        override fun displayInfo() {
+            super.displayInfo()
+            println("Car - Number of doors: $numDoors")
+        }
+
+    }
+
+    class Bike(speed: Int, color: String, val numWheels: Int) : Vehicle(speed, color) {
+        override fun displayInfo() {
+            super.displayInfo()
+            println("Bike - Number of wheels: $numWheels")
+        }
+
+    }
+
+}
