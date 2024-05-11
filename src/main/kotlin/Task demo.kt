68e9ffa97c654bbd4a4e990c0c4c1199ad448add
Reyan Jahangir <reyanjr@mail.ru>
2024-05-11 12:51:55 +0300

    Задание

---
+++ src/main/kotlin/Task demo.kt
@@ -0,0 +1,23 @@
+package org.example
+
+abstract class Appliance {
+    var pluggedIn = true
+    abstract val color: String
+    abstract fun consumePower()
+}
+
+class CoffeeMaker: Appliance() {
+    override val color = ""
+    var coffeeLeft = false
+
+    override fun consumePower(){
+        println("Consuming Power")
+    }
+
+    fun fillWithWater(){
+        println("Fill with water")
+    }
+    fun makeCoffee(){
+        println("Make the coffee")
+    }
+}
